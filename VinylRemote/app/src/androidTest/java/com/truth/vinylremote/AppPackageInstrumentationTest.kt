60891144899c2ca9ec8f7795544928577d91a97b
Minki-Cho <minji.cho@digipen.edu>
2026-03-25 15:49:07 -0700

    test: add baseline android instrumentation coverage

---
+++ VinylRemote/app/src/androidTest/java/com/truth/vinylremote/AppPackageInstrumentationTest.kt
@@ -0,0 +1,16 @@
+package com.truth.vinylremote
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class AppPackageInstrumentationTest {
+    @Test
+    fun appContext_hasExpectedPackageName() {
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.truth.vinylremote", appContext.packageName)
+    }
+}
